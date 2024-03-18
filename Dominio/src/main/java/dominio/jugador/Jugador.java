77cae8ea381cf5af8f3c4357aef3646815ef5e2c
AracelyC <aracely.campa233215@potros.itson.edu.mx>
2024-03-18 12:31:40 -0700

    Se agregaron las clases del diagrama

---
+++ Dominio/src/main/java/dominio/jugador/Jugador.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio.jugador;
+
+/**
+ *
+ * @author arace
+ */
+public class Jugador {
+    String nombre;
+    int score;
+
+    public Jugador() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "nombre=" + nombre + ", score=" + score + '}';
+    }
+    
+    
+}
