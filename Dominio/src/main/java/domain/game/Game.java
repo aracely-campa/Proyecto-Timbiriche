ec4e530c64815f70bd9556bf531fbfbf707a3312
LuisReynoso233531 <luis.reynoso233531@potros.itson.edu.mx>
2024-04-02 17:58:36 -0700

    Dominio Partida

--- Dominio/src/main/java/domain/game/Game.java
+++ Dominio/src/main/java/domain/game/Game.java
@@ -4,10 +4,44 @@
  */
 package domain.game;
 
+import domain.player.Player;
+
 /*
  *
  * @author arace
  */
 public class Game {
-    
+
+    private Player player;
+    private Board board;
+
+    public Game() {
+    }
+
+    public Game(Player player, Board board) {
+        this.player = player;
+        this.board = board;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "player=" + player + ", board=" + board + '}';
+    }
+
 }
